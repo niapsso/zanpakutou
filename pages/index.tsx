ab3071232410c3bb3705ca1cbbc0b6244cb3f888
andre <andrepassoni98@gmail.com>
2022-07-26 13:59:56 -0300

    feat: basic integration w aws lambda to email sending

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState, FormEvent } from "react";
 
 const Home: NextPage = () => {
+  const [contact, setContact] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const payload = {
+      contact,
+      message,
+    };
+
+    const res = await fetch("/api/mail/", {
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(payload),
+    })
+      .then((res) => res.json())
+      .catch(console.log);
+
+    console.log(res);
+  };
+
   return (
     <div>
       <Head>
@@ -10,6 +36,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello world, from next!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="contact"
+          onChange={({ target: { value } }) => setContact(value)}
+          value={contact}
+        />
+        <input
+          placeholder="message"
+          onChange={({ target: { value } }) => setMessage(value)}
+          value={message}
+        />
+        <button type="submit">send</button>
+      </form>
     </div>
   );
 };
---
+++ utils/sendMail.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+
+interface sendMailProps {
+  contact: string;
+  message: string;
+}
+
+const sendMail = async (payload: sendMailProps) =>
+  await fetch(process.env.LAMBDA_API_URL as string, {
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .catch(console.log);
+
+export default sendMail;
